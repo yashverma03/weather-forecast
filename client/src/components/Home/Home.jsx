67c095e5a51703d05c6052f1947da7a99c5c5cae
yashverma03 <vermayash2003@gmail.com>
2023-09-09 12:25:34 +0530

    updated Home component

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { FormControl, Button, Form } from 'react-bootstrap';
 import styles from './Home.module.css';
+import API_URL from '../../config/api';
+import axios from 'axios';
 import WeatherCondition from '../WeatherCondition/WeatherCondition';
 import WeatherForecast from '../WeatherForecast/WeatherForecast';
 
@@ -8,17 +10,31 @@ const Home = () => {
   const [cityName, setCityName] = useState('');
   const [weatherData, setWeatherData] = useState(null);
 
-  const handleOnSubmit = (e) => {
+  const handleOnSubmit = async (e) => {
     e.preventDefault();
 
+    if (cityName.trim() !== '') {
+      try {
+        const repsonse = await axios.get(`${API_URL}?cityName=${cityName}`);
+        const data = repsonse.data;
 
+        setWeatherData(data);
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+      }
+    }
   };
 
   return (
     <div>
       <div className={styles.formContainer}>
         <Form className={styles.form} onSubmit={handleOnSubmit}>
-          <FormControl className={styles.input} placeholder='Enter City name ...' />
+          <FormControl
+            className={styles.input}
+            placeholder='Enter City name ...'
+            value={cityName}
+            onChange={(e) => setCityName(e.target.value)}
+          />
 
           <Button className={styles.button} variant='primary' type='submit'>
             Get Weather
@@ -28,8 +44,8 @@ const Home = () => {
 
       {weatherData && (
         <>
-          <WeatherCondition />
-          <WeatherForecast />
+          <WeatherCondition data={weatherData.currentWeather} />
+          <WeatherForecast data={weatherData.forecastData} />
         </>
       )}
     </div>
