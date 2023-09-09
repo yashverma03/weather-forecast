8ccd5898cf9f83a90690a2047910bcb452869d8a
yashverma03 <vermayash2003@gmail.com>
2023-09-09 13:50:21 +0530

    updated all components

--- client/src/components/WeatherCondition/WeatherCondition.jsx
+++ client/src/components/WeatherCondition/WeatherCondition.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import styles from './WeatherCondition.module.css';
 
-const WeatherCondition = () => {
+const WeatherCondition = ({ data }) => {
+  if (!data) {
+    return null;
+  }
+
+  const { main, weather } = data;
+  const { temp, humidity } = main;
+  const weatherDescription = weather[0].description;
+
   return (
-    <div>WeatherCondition</div>
+    <div className={styles.container}>
+      <h2 className={styles.heading}>Current Weather</h2>
+
+      <div className={styles.textContainer}>
+        <p className={styles.text}>Temperature: {temp}°C</p>
+        <p className={styles.text}>Humidity: {humidity}%</p>
+        <p className={styles.text}>Description: {weatherDescription}</p>
+      </div>
+    </div>
   );
 };
 
--- client/src/components/WeatherForecast/WeatherForecast.jsx
+++ client/src/components/WeatherForecast/WeatherForecast.jsx
@@ -1,9 +1,61 @@
 import React from 'react';
 import styles from './WeatherForecast.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun, faCloud, faCloudSun, faCloudRain, faSnowflake, faWind, } from '@fortawesome/free-solid-svg-icons';
 
-const WeatherForecast = () => {
+const getWeatherIcon = (description) => {
+  switch (description.toLowerCase()) {
+    case 'clear sky':
+      return <FontAwesomeIcon icon={faSun} />;
+
+    case 'few clouds':
+    case 'scattered clouds':
+    case 'broken clouds':
+      return <FontAwesomeIcon icon={faCloudSun} />;
+
+    case 'cloudy':
+      return <FontAwesomeIcon icon={faCloud} />;
+
+    case 'light rain':
+    case 'moderate rain':
+    case 'heavy intensity rain':
+      return <FontAwesomeIcon icon={faCloudRain} />;
+
+    case 'snow':
+      return <FontAwesomeIcon icon={faSnowflake} />;
+
+    default:
+      return <FontAwesomeIcon icon={faWind} />;
+  }
+};
+
+const WeatherForecast = ({ data }) => {
+  if (!data) {
+    return null;
+  }
+
+  const { list } = data;
+
   return (
-    <div>WeatherForecast</div>
+    <div className={styles.container}>
+      <h2 className={styles.heading}>5-Day Weather Forecast</h2>
+
+      <div className={styles.headingRow}>
+        <p className={styles.headingText}>Date</p>
+        <p className={styles.headingText}>Temperature</p>
+        <p className={styles.headingText}>Icon</p>
+      </div>
+
+      <ul className={styles.listContainer}>
+        {list.filter((forecast, index) => index % 8 === 0).map((forecast, index) => (
+          <li className={styles.list} key={index}>
+            <p className={styles.text}>{forecast.dt_txt.split(' ')[0]}</p>
+            <p className={styles.text}>{forecast.main.temp}° C</p>
+            <p className={styles.image}>{getWeatherIcon(forecast.weather[0].description)}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
