f34935ea31c89012997d6102f8f061f9eae2c7c7
yashverma03 <vermayash2003@gmail.com>
2023-09-09 19:10:29 +0530

    updated api url

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,4 +1,7 @@
-// URL of backend server
-const API_URL = 'http://localhost:4000';
+// URL of backend server (hosted)
+const API_URL = 'https://weather-forecast-0fg1.onrender.com';
+
+// URL of backend server (local)
+// const API_URL = 'http://localhost:4000';
 
 export default API_URL;
